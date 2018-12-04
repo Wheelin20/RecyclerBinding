1bed0e5dcddd10a1161b25cfe1fd9dc22e05246b
Kevin Emigh <kevin.emigh@ohiohealth.com>
2018-12-04 17:31:33 -0500

    initial commit

---
+++ app/src/main/java/com/waldoms/recyclerbinding/DogBreedsActivity.kt
@@ -0,0 +1,12 @@
+package com.waldoms.recyclerbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DogBreedsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_breeds)
+    }
+}
